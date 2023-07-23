2b47759c8dc05310e81ca022c4670d25014efb39
triplemzim <triplemzim@gmail.com>
2023-07-24 03:49:33 +0800

    initial setup complete - react + vite + airbnb + prettier + typescript

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
+
+export default WrappedApp;
