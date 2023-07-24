ed59239be3c0eb44c1f49e91a2c59ad0d965aa85
triplemzim <triplemzim@gmail.com>
2023-07-25 01:56:57 +0800

    added few other libraries

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline text-center text-red-500">
+      Hello world!
+    </h1>
+  );
 }
 
 export default Home;
