16e834aa8ec7c4f7f76cc60d9dd605ac1561e86f
Muntaqeem <muntaqeem.labbaeik@gmail.com>
2023-08-17 00:05:02 +0600

    Auth implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
+import Axios from '../lib/axios';
+
+const axios = new Axios();
+
+console.log(axios);
+
 function Home() {
   return (
     <h1 className="text-3xl font-bold underline text-center text-red-500">
